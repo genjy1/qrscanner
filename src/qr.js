45a6a56446afd280d22036ae11d2baafb16753c3
Daniil <idontknower1997@gmail.com>
2025-01-17 10:39:50 +0300

    Вывод в html

--- src/qr.js
+++ src/qr.js
@@ -1,5 +1,8 @@
+const resultWrapper = document.querySelector('#result')
 if (!('BarcodeDetector' in window)) {
     console.error('Barcode Detection API is not supported in this browser.');
+    resultWrapper.textContent = 'Barcode Detection API is not supported in this browser.'
 } else {
     console.log('Barcode Detection API is supported.');
+    resultWrapper.textContent = 'Barcode Detection API is supported.'
 }
