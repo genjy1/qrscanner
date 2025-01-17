e669950572b3c0afb6751fc5d726d790e259a70f
Daniil <idontknower1997@gmail.com>
2025-01-17 11:02:53 +0300

    Поправил имя класса

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const videoElement = document.querySelector('#video');
     const resultElement = document.querySelector('#result');
     const startButton = document.querySelector('#startScan');
-    const formatElement = document.querySelector('.format')
+    const formatElement = document.querySelector('.formats')
 
     startButton.addEventListener('click', () => {
         startBarcodeScanner(videoElement, resultElement, formatElement);
