2e12fd448cb199c6a02af76ed78a8ad620034b7d
Daniil <idontknower1997@gmail.com>
2025-01-17 10:38:37 +0300

    Проверка на BarcodeDetectorAPI

--- src/qr.js
+++ src/qr.js
@@ -1,32 +1,5 @@
-import Quagga from 'quagga';
-
-const resultWrapper = document.querySelector('#result')
-
-Quagga.init(
-    {
-        inputStream: {
-            type: 'LiveStream',
-            target: document.querySelector('#scanner'),
-            constraints: {
-                facingMode: 'environment', // Использование задней камеры
-            },
-        },
-        decoder: {
-            readers: ['code_128_reader', 'ean_reader'], // Добавьте тип вашего линейного кода
-        },
-    },
-    (err) => {
-        if (err) {
-            console.error('Ошибка инициализации Quagga:', err);
-            resultWrapper.textContent = err;
-            return;
-        }
-        console.log('Quagga инициализирован');
-        Quagga.start();
-    }
-);
-
-Quagga.onDetected((data) => {
-    console.log('Считан код:', data.codeResult.code);
-    alert(`Результат: ${data.codeResult.code}`);
-});
+if (!('BarcodeDetector' in window)) {
+    console.error('Barcode Detection API is not supported in this browser.');
+} else {
+    console.log('Barcode Detection API is supported.');
+}
